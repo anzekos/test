18a5acb90ada193c1cba80899290189f7d34d8b6
Anže Kos <130596116+anzekos@users.noreply.github.com>
2026-06-17 11:17:08 +0200

    Delete backend directory

--- backend/fetch_json.py
+++
@@ -1,4 +0,0 @@
-import urllib.request, json
-with open("zako5944.json", "w", encoding="utf-8") as f:
-    r = urllib.request.urlopen("https://pisrs.si/api/rezultat/zbirka/id/ZAKO5944").read().decode("utf-8")
-    json.dump(json.loads(r), f, indent=2, ensure_ascii=False)
